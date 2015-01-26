0bf541e3da122f61067ca9c95f49eca53df4c384
Zhiya Zuo <zhiyazuo@gmail.com>
2015-01-26 15:47:21 -0600

    complete Dirichlet

--- distributions.py
+++ distributions.py
@@ -1,6 +1,21 @@
 import random
 import math
+import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+def GammaFunc(x):
+    return math.gamma(x)
+
+def BetaFunc(alphas=[1,1,1]):
+    alpha0 = sum(alphas)
+    denominator = float(GammaFunc(alpha0))
+    nominator = 0
+    for alpha in alphas:
+        nominator *= GammaFunc(alpha)
+
+    return nominator/denominator
+
 
 def Gaussian(n=1000, mu=0, sigma=1, plot=False):
     ''' Generate a Gaussian Distribution;
@@ -58,6 +73,32 @@ def Gamma(n=1000, alpha=1, beta=1, plot=False):
         plt.title("Gamma Histogram")
         plt.xlabel("Value")
         plt.ylabel("Frequency")
+        plt.show()
+
+    return samples
+
+def Dirichlet(n=1000, alphas=[1,1,1]):
+
+    ''' Generate a Dirichlet Distribution;
+        Default: #points is 1000;
+                 plot 3-dim space;
+                 2-dim simplex with uniform distribution;
+    '''
+
+    dim = len(alphas)
+    samples = np.array([([None] * dim) for row in xrange(n)])
+
+    for i in range(n):
+        for j in range(dim): 
+            alpha = alphas[j]
+            samples[i, j] = random.gammavariate(alpha, 1)
+        samples[i, :] = samples[i, :]/sum(samples[i, :])
+
+
+    if dim==3:
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(samples[:, 0], samples[:, 1] , samples[:, 2])
         plt.show()
 
     return samples
