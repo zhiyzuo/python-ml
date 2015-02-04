57b64107cd470c8786c9b847a379dd14caacc4fa
Zhiya Zuo <zhiyazuo@gmail.com>
2015-02-04 13:11:29 -0600

    first commit of data.py: vector class

--- data.py
+++ data.py
@@ -1,6 +1,92 @@
-import helpers
-class Feature:
+from helpers import *
+from helpers.exceptions import *
+
+class Vector:
+    def __init__(self, vector=[]):
+        self.vector = vector
+        self.dimension = len(vector)
+        
+    def __mul__(self, anotherVector):
+        try:
+            if len(anotherVector) != len(self):
+                raise DimensionError(self, anotherVector)
+                return sum([self[i]*anotherVector[i] for i in range(self.dimension)])
+        except DimensionError as e:
+            print e.msg
+
+    def __rmul__(self, anotherVector):
+        return self*anotherVector
+
+    def __imul__(self, anotherVector):
+        try:
+            if len(anotherVector) != len(self):
+                raise DimensionError(self, anotherVector)
+            self.vector = [self.vector[i] * anotherVector[i] for i in range(len(self))]
+        except DimensionError as e:
+            print e.msg
+
+    def __add__(self, anotherVector):
+        try:
+            if len(anotherVector) != len(self):
+                raise DimensionError(self, anotherVector)
+            return Vector([self.vector[i] + anotherVector[i] for i in range(len(self))])
+        except DimensionError as e:
+            print e.msg
+
+    def __radd__(self, anotherVector):
+        return (self + anotherVector)
+
+    def __iadd__(self, anotherVector):
+        try:
+            if len(anotherVector) != len(self):
+                raise DimensionError(self, anotherVector)
+            self.vector = [self.vector[i] + anotherVector[i] for i in range(len(self))]
+        except DimensionError as e:
+            print e.msg
+
+    def __sub__(self, anotherVector):
+        try:
+            if len(anotherVector) != len(self):
+                raise DimensionError(self, anotherVector)
+            return Vector([self.vector[i] - anotherVector[i] for i in range(len(self))])
+        except DimensionError as e:
+            print e.msg
+
+    def __rsub__(self, anotherVector):
+        return (self - anotherVector)
+
+    def __isub__(self, anotherVector):
+        try:
+            if len(anotherVector) != len(self):
+                raise DimensionError(self, anotherVector)
+            self.vector = [self.vector[i] - anotherVector[i] for i in range(len(self))]
+        except DimensionError as e:
+            print e.msg
+
+    def __getitem__(self, index):
+        return self.vector[index]
+
+    def __eq__(self, anotherVector):
+        for index in range(self.dimension):
+            if self.vector[index] != anotherVector[index]:
+                return False
+        return True
+
+    def __getslice__(self, i, j):
+        return self.vector[i:j]
+
+    def __len__(self):
+        return self.dimension
+
+    def getDimension(self):
+        return self.dimension
+
+
+
+
 #TODO
+'''
+class Feature:
     def __init__(self, data):
     # data should be in the form of list or numpy array
         self.features = data
@@ -16,4 +102,5 @@ class Feature:
         for col in columns:
             # extract a specified column from feature space
             feature = [[sample[col]] for sample in self.features]
-            feature = helpers.normalize(feature, method)
+            feature = preprocessing.normalize(feature, method)
+'''
