e67f97bbc856f32d46504594b89753584c300d2f
Zhiya Zuo <zhiyazuo@gmail.com>
2015-02-05 23:57:30 -0600

    Add Feature Class; Delete Vector/Matrix Class; Moidify Normalization func

--- pyml/data.py
+++ pyml/data.py
@@ -1,124 +1,25 @@
-from helpers import *
+from helpers.preprocessing import *
 from helpers.exceptions import *
+import numpy as np
 
-class Vector:
-# {{{ Vector Class
-    def __init__(self, vector=[]):
-        self.vector = vector
-        self.dimension = len(vector)
-        
-    def __mul__(self, anotherVector):
-        try:
-            if len(anotherVector) != len(self):
-                raise DimensionError(self, anotherVector)
-            return sum([self[i]*anotherVector[i] for i in range(self.dimension)])
-        except DimensionError as e:
-            print e.msg
-
-    def __rmul__(self, anotherVector):
-        return self*anotherVector
-
-    def __imul__(self, anotherVector):
-        try:
-            if len(anotherVector) != len(self):
-                raise DimensionError(self, anotherVector)
-            self.vector = [self.vector[i] * anotherVector[i] for i in range(len(self))]
-        except DimensionError as e:
-            print e.msg
-
-    def __add__(self, anotherVector):
-        try:
-            if len(anotherVector) != len(self):
-                raise DimensionError(self, anotherVector)
-            return Vector([self.vector[i] + anotherVector[i] for i in range(len(self))])
-        except DimensionError as e:
-            print e.msg
-
-    def __radd__(self, anotherVector):
-        return (self + anotherVector)
-
-    def __iadd__(self, anotherVector):
-        try:
-            if len(anotherVector) != len(self):
-                raise DimensionError(self, anotherVector)
-            self.vector = [self.vector[i] + anotherVector[i] for i in range(len(self))]
-        except DimensionError as e:
-            print e.msg
-
-    def __sub__(self, anotherVector):
-        try:
-            if len(anotherVector) != len(self):
-                raise DimensionError(self, anotherVector)
-            return Vector([self.vector[i] - anotherVector[i] for i in range(len(self))])
-        except DimensionError as e:
-            print e.msg
-
-    def __rsub__(self, anotherVector):
-        return (self - anotherVector)
-
-    def __isub__(self, anotherVector):
-        try:
-            if len(anotherVector) != len(self):
-                raise DimensionError(self, anotherVector)
-            self.vector = [self.vector[i] - anotherVector[i] for i in range(len(self))]
-        except DimensionError as e:
-            print e.msg
-
-    def __getitem__(self, index):
-        try:
-            return self.vector[index]
-        except IndexError as e:
-            print e
-
-    def __eq__(self, anotherVector):
-        for index in range(self.dimension):
-            if self.vector[index] != anotherVector[index]:
-                return False
-        return True
-
-    def __getslice__(self, i, j):
-        try:
-            return tuple(self.vector[i:j])
-        except IndexError as e:
-            print e
-
-    def __len__(self):
-        return self.dimension
-
-    def __str__(self):
-        return "Vector({})".format(",".join(str(element) for element in self.vector))
-
-    def __repr__(self):
-        return "Vector({})".format(",".join(str(element) for element in self.vector))
-
-    def getDimension(self):
-        return self.dimension
-
-# }}}
-
-#TODO
-class Matrix(Vector):
-# {{{ Matrix Class
-    def __init__(self, data):
-        self.matrix = None
-# }}}
-
-'''
 class Feature:
     def __init__(self, data):
-    # data should be in the form of list or numpy array
-        self.features = data
-        self.dimension = len(data[0])
-        self.size = len(data)
-    def getDimension(self):
+    # data should be in the form of list or numpy matrix
+        try:
+            if len(data) < 1:
+                raise EmptyErro
+            self.features = np.asmatrix(data)
+            self.dimension = len(data[0])
+            self.size = len(data)
+        except EmptyError as e:
+            print e
+
+    def get_dimension(self):
         return self.dimension
-    def getFeatures(self):
+
+    def get_features(self):
         return self.features
-    def normalize(self, method="linear", columns=None):
-        if columns == None:
-            columns = range(0, self.dimension)
-        for col in columns:
-            # extract a specified column from feature space
-            feature = [[sample[col]] for sample in self.features]
-            feature = preprocessing.normalize(feature, method)
-'''
+
+    def get_normalized(self, columns, method="linear"):
+    # return a new Feature object that is normalized
+        return Feature(normalize(self.features, columns, method))
--- pyml/helpers/preprocessing.py
+++ pyml/helpers/preprocessing.py
@@ -1,22 +1,35 @@
 from operations import *
+import numpy as np
 
-def normalize(vector, method="linear"):
+def normalize(matrix, columns, method="linear"):
+# input should be a numpy matrix
+    normalized_matrix = np.matrix([[] for i in range(matrix.shape[1])])
     if method == "linear":
-        minVal = float(min(vector))
-        scale = - minVal + max(vector)
-        try:
-            return [(xi - minVal)/scale for xi in vector]
-        except ZeroDivisionError:
-            print "Maximum and minimun are the same value!"
-            return
+        for col in range(matrix.shape[1]):
+            if col in columns:
+                vector = matrix[:, col][:]
+                minVal = float(min(vector))
+                scale = - minVal + max(vector)
+                try:
+                    normalized_matrix = np.concatenate((normalized_matrix, np.array([[(vector[i, 0] - minVal)/scale] for i in range(vector.shape[0])])), axis=1)
+                except ZeroDivisionError:
+                    print "Maximum and minimun are the same value!"
+                    continue
+            else:
+                normalized_matrix = np.concatenate((normalized_matrix, vector), axis=1)
 
+    '''
+        #TODO: feels not right
     elif method.lower() =="z" or method.lower() == "z-transform":
-        meanVal = mean(vector)
-        stdVal = std(vector)
-        try:
-            return [(xi - meanVal)/stdVal for xi in vector]
-        except ZeroDivisionError:
-            print "All points are of the same value!"
-            return
+        for col in columns:
+            vector = matrix[:, col]
+            meanVal = mean(vector)
+            stdVal = std(vector)
+            try:
+                return [(xi - meanVal)/stdVal for xi in vector]
+            except ZeroDivisionError:
+                print "All points are of the same value!"
+                return
+    '''
 
-
+    return normalized_matrix
