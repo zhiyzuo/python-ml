134927bf18d4a3802a08ac11caab88d029e77591
Zhiya Zuo <zhiyazuo@gmail.com>
2015-02-08 16:43:42 -0600

    finish target class

--- pyml/data.py
+++ pyml/data.py
@@ -9,7 +9,7 @@ class Feature:
     # data should be in the form of list or numpy matrix
         try:
             if len(data) < 1:
-                raise EmptyErro
+                raise EmptyError
 
             self.feature_space = np.asmatrix(data)
             self.dimension = len(data[0])
@@ -21,7 +21,7 @@ class Feature:
                 self.header = np.asarray(header)
 
         except EmptyError as e:
-            print e
+            raise
 
     def get_header(self):
         return tuple(self.header[:])
@@ -40,41 +40,58 @@ class Feature:
     # return a new Feature object whose specified columns are discretized
         if bins == None:
             bins = [math.sqrt(self.size) for col in columns]
-        return Feature(discretize(self.feature_space, columns, bins=[math.sqrt(self.size) for col in columns]))
+        return Feature(discretize(self.feature_space, columns, bins))
 # }}}
 
-'''
 class Target:
 # {{{ Target Class
-    def __init__(self, target, header=np.array([])):
+    def __init__(self, targets, header=np.array([])):
     # target should be a column
         try:
-            if len(data) < 1:
-                raise EmptyErro
+            if len(targets) < 1:
+                raise EmptyError
 
-            self.target = target
-            self.size = len(data)
+            self.targets = np.asmatrix(targets)
+            self.size = self.targets[0].size
+            self.dimension = self.targets.shape[0]
 
             if len(header) < 1:
-                self.header = np.array(["target"])
-            else:
+                self.header = np.array(["target" for i in range(self.dimension)])
+            elif len(header) != self.dimension:
+                raise DimensionError
+            elif type(header) in [list, np.ndarray]:
                 self.header = np.asarray(header)
+            else:
+                self.header = np.asarray([header])
 
-        except EmptyError as e:
-            print e
+        except EmptyError:
+            raise
+
+        except DimensionError:
+            raise
+
 
     def get_header(self):
-        return tuple(self.header[:])
+        return self.header[:]
 
-    def get_dimension(self):
-        return self.dimension
+    def get(self, columns=None):
+        try:
+            if columns == None:
+                return self.targets[:]
+            elif type(columns) == int:
+                return self.targets[:, columns]
+            elif type(columns) == np.ndarray or type(columns) == list:
+                return np.column_stack(self.targets[:, col] for col in columns)
+            else:
+                raise TypeError
+        except TypeError:
+            print "Please either enter an integer or array for specified columns of this Target object!"
 
-    def get(self):
-        return self.target[:]
-
-    #TODO
-    def get_discretized(self, bins=math.sqrt(self.size)):
-    # return a new Target object that is discretized
-        return
+    def get_discretized(self, columns, bins=None):
+    # return a new Target object whose specified columns are discretized
+        if bins == None:
+            bins = [math.sqrt(self.size) for i in range(self.size)]
+        elif type(bins) == int:
+            bins = [bins for i in range(self.size)]
+        return Target(discretize(self.targets, columns, bins), self.header)
 # }}}
-'''
