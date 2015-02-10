a091368886f1b2fa425228400b3eea9b9e0b6050
Zhiya Zuo <zhiyazuo@gmail.com>
2015-02-09 23:15:25 -0600

    set up an initial framework

---
+++ pyml/graph/pagerank.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+'''
+Page Rank Algorithm for Graph
+** Page, Lawrence, et al. "The PageRank citation ranking: Bringing order to the web." (1999). **
+'''
+
+#TODO
+def _power_iteration(r, M, random=True, beta=0.8):
+    '''
+    default: Google Formulation and beta=0.8
+    '''
---
+++ pyml/predictors/__init__.py
@@ -0,0 +1,2 @@
+__all__ = ['decision_trees', 'linear_regression', 'logistic_regression',
+            'naive_bayes', 'svm']
