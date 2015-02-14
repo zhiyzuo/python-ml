f9ad90fba1ba2514a21571879b8450eeef05abdf
Zhiya Zuo <zhiyazuo@gmail.com>
2015-02-14 15:35:37 -0600

    add jaccard distance

--- pyml/distances/euclidean_distance.py
+++ pyml/distances/euclidean_distance.py
@@ -1,32 +1,33 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 
-def euclidean_distance(x1, x2, r=2):
+def euclidean_distance(p1, p2, r=2):
 # {{{ Euclidean distance
-    ''' Compute Euclidean distance between x1 and x2;
+    ''' Input objects should be two points;
+        Compute Euclidean distance between p1 and p2;
         Default is L2-norm
         r == 'inf' indicates L∞-Norm
     '''
 
     try:
-        if (type(r) not in  (int, str)) or (type(x1) not in (np.ndarray, list, tuple)) or \
-                (type(x1) not in (np.ndarray, list, tuple)):
+        if (type(r) not in  (int, str)) or (type(p1) not in (np.ndarray, list, tuple)) or \
+                (type(p1) not in (np.ndarray, list, tuple)):
             raise TypeError("Input data invalid!")
         #TODO: raise possible errors
         else:
             #  L∞-Norm 
             if r == 'inf':
                 distance = 0
-                for i in range(len(x1)):
-                    x1i, x2i = x1[i], x2[i]
-                    if distance < abs(x1i-x2i):
-                        distance = abs(x1i-x2i)
+                for i in range(len(p1)):
+                    p1i, p2i = p1[i], p2[i]
+                    if distance < abs(p1i-p2i):
+                        distance = abs(p1i-p2i)
             # Lr-Norm
             else:
                 sumation = 0
-                for i in range(len(x1)):
-                    x1i, x2i = x1[i], x2[i]
-                    sumation += abs(x1i-x2i)**r
+                for i in range(len(p1)):
+                    p1i, p2i = p1[i], p2[i]
+                    sumation += abs(p1i-p2i)**r
                 distance = float(sumation)**(1./r)
             return distance
     except TypeError as e:
