e26c738fdec5a091728af5a116ea2982b991b280
Zhiya Zuo <zhiyazuo@gmail.com>
2015-12-30 18:58:09 -0600

    k-fold cross validation

--- pyml/cross_validation.py
+++ pyml/cross_validation.py
@@ -13,6 +13,16 @@ def leave_one_out(n):
         tr_ = np.array([idx for idx in indices if idx != index])
         yield (tr_, te_)
 
-def k_fold(data, k=5):
-    pass
+def k_fold(n, k=5):
+    '''
+        Partition datasets for use of K-Fold Cross Validation
+    '''
+
+    indices = np.arange(n)
+    np.random.shuffle(indices)
+    k_folds = np.split(indices, k)
+    for index in k_folds:
+        te_ = np.array([index]).flatten()
+        tr_ = np.array([idx for idx in k_folds if (idx != index).all()]).flatten()
+        yield (tr_, te_)
 
